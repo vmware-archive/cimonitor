78de5dba0520fbf6966593f88f287da3a8305f34
Joseph Moore <joe.moore@gmail.com>
2010-03-28 11:30:12 -0700

    Mostly works, but in-progress bulids record a failed build status.

--- app/models/integrity_status_parser.rb
+++ app/models/integrity_status_parser.rb
@@ -16,15 +16,15 @@ class IntegrityStatusParser < StatusParser
     def status(content, project)
       status_parser = self.new
       begin
-        latest_build = Nokogiri::XML.parse(content).css('#last_build').first
+        doc = Nokogiri::XML.parse(content)
+        latest_build = doc.css('#last_build').first
         status_parser.success = !!(latest_build.attribute('class').value =~ /success/i)
-        build_number = latest_build.css('form').first.attribute('action').value.split('/').last
-        status_parser.url = "#{project.feed_url}/build/#{build_number}"
-        
+        build_number = doc.css('#previous_builds li a').first.attribute('href').value.split('/').last
+        status_parser.url = "#{project.feed_url}/builds/#{build_number}"
         pub_date = Time.parse(latest_build.css('.when').first.attribute('title').value)
         status_parser.published_at = (pub_date == Time.at(0) ? Clock.now : pub_date).localtime
-      rescue
-        #die quietly
+      rescue => e
+        #raise e # uncomment to debug, otherwise die silently
       end
       status_parser
     end
