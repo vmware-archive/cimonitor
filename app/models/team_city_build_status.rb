110e2847bdb00f7e66b1ca714ddc6a86934fee86
Dirk Kelly & Matt Parker <pair+dirk+matt@pivotallabs.com>
2012-03-27 10:59:52 -0400

    TeamCityBuildStatus returns current build status

---
+++ app/models/team_city_build_status.rb
@@ -0,0 +1,47 @@
+class TeamCityBuildStatus
+  attr_reader :build
+  # "#{base_url}/app/rest/builds?locator=running:all,buildType:(id:#{id})"
+
+  def initialize(build)
+    @build = build
+  end
+
+  def fetch
+    @building = build_response["running"] == "true"
+    @green = build_response["status"] == "SUCCESS"
+    @online = true
+
+  rescue Net::HTTPError
+    @online = false
+  end
+
+  def online?
+    @online
+  end
+
+  def green?
+    online? && !building? && @green
+  end
+
+  def red?
+    online? && !green? && !building?
+  end
+
+  def building?
+    online? && @building
+  end
+
+  private
+  def build_response
+    @build_response || fetch_build_response
+  end
+
+  def fetch_build_response
+    response = UrlRetriever.retrieve_content_at(build.url, build.username, build.password)
+    response = Hash.from_xml response
+
+    @build_response = response['builds']['build']
+    @build_response = @build_response.first if @build_response.kind_of? Array
+    @build_response
+  end
+end
---
+++ spec/models/team_city_build_status_spec.rb
@@ -0,0 +1,141 @@
+require "spec_helper"
+
+describe TeamCityBuildStatus do
+
+  describe "#initialize" do
+    let(:build) { double(:build) }
+    it "requires a build" do
+      status = TeamCityBuildStatus.new(build)
+      status.build.should == build
+    end
+  end
+
+  describe "#fetch" do
+    let(:build) { double(:build, :url => "http", :username => "foo", :password => "bar") }
+    let(:status) { TeamCityBuildStatus.new(build) }
+    let(:response_xml) do
+      <<-XML
+        <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+        <builds count="1">
+          <build id="1" number="1" status="SUCCESS"/>
+          <build id="2" number="2" status="SUCCESS"/>
+        </builds>
+      XML
+    end
+
+    before do
+      UrlRetriever.stub(:retrieve_content_at).
+        with(build.url, build.username, build.password).
+        and_return(response_xml)
+    end
+
+    it "uses the urlretriever to fetch the status" do
+      UrlRetriever.should_receive(:retrieve_content_at).
+        with(build.url, build.username, build.password).
+        and_return(response_xml)
+      status.fetch
+    end
+
+    context "green response" do
+      before { status.fetch }
+
+      it "assigns online? true" do
+        status.online?.should be_true
+      end
+
+      it "assigns green? true" do
+        status.green?.should be_true
+      end
+
+      it "assigns building? false" do
+        status.building?.should be_false
+      end
+
+      it "assigns red? false" do
+        status.red?.should be_false
+      end
+    end
+
+    context "building response" do
+      let(:response_xml) do
+        <<-XML
+          <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+          <builds count="1">
+            <build id="1" number="1" running="true" status="SUCCESS"/>
+          </builds>
+        XML
+      end
+
+      before { status.fetch }
+
+      it "assigns online? true" do
+        status.online?.should be_true
+      end
+
+      it "assigns green? false" do
+        status.green?.should be_false
+      end
+
+      it "assigns building? true" do
+        status.building?.should be_true
+      end
+
+      it "assigns red? false" do
+        status.red?.should be_false
+      end
+    end
+
+    context "red response" do
+      let(:response_xml) do
+        <<-XML
+          <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+          <builds count="1">
+            <build id="1" number="1" status="#{['ERROR','FAILURE'].sample}"/>
+            <build id="2" number="2" status="SUCCESS"/>
+          </builds>
+        XML
+      end
+
+      before { status.fetch }
+
+      it "assigns online? true" do
+        status.online?.should be_true
+      end
+
+      it "assigns green? false" do
+        status.green?.should be_false
+      end
+
+      it "assigns building? false" do
+        status.building?.should be_false
+      end
+
+      it "assigns red? true" do
+        status.red?.should be_true
+      end
+    end
+
+    context "invalid response" do
+      before do
+        UrlRetriever.stub(:retrieve_content_at).and_raise(Net::HTTPError.new("foo", "jkfldsaj"))
+        status.fetch
+      end
+
+      it "assigns online? false" do
+        status.online?.should be_false
+      end
+
+      it "assigns green? false" do
+        status.green?.should be_false
+      end
+
+      it "assigns building? false" do
+        status.building?.should be_false
+      end
+
+      it "assigns red? false" do
+        status.red?.should be_false
+      end
+    end
+  end
+end
